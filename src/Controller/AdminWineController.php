e29d2e7269d99250f8c22806789f6dca1d44b01a
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2020-04-28 14:47:58 +0200

    add wine

--- src/Controller/AdminWineController.php
+++ src/Controller/AdminWineController.php
@@ -13,4 +13,47 @@ class AdminWineController extends AbstractController
 
         return $this->twig->render('AdminWine/index.html.twig', ['wines' => $wines]);
     }
+
+    public function add()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $wine = array_map('trim', $_POST);
+
+            $errors = $this->validation($wine);
+            if(empty($errors)) {
+                $wineManager = new WineManager();
+                $wineManager->insert($wine);
+                header('Location: /adminWine/index');
+            }
+        }
+
+        return $this->twig->render('AdminWine/add.html.twig', ['errors'=>$errors ?? [], 'wine'=> $wine ?? []]);
+    }
+
+    private function validation(array $wine): array
+    {
+        $wineNameLength = $producerLength = 255;
+        $startYear = 1900;
+        $now = new \DateTime();
+        $endYear = $now->format('Y');
+
+        if (empty($wine['name'])) {
+            $errors[] = 'Le nom du vin est requis';
+        } elseif (strlen($wine['name']) > $wineNameLength) {
+            $errors[] = 'Le nom du vin doit faire moins de ' . $wineNameLength;
+        }
+
+        if (isset($wine['producer']) && strlen($wine['producer']) > $producerLength) {
+            $errors[] = 'Le nom du producteur doit faire moins de ' . $producerLength;
+        }
+
+        if (isset($wine['year']) && !is_numeric($wine['year'])) {
+            $errors[] = 'Le format de l\'année doit être un nombre entier';
+        } elseif ($wine['year'] < $startYear || $wine['year'] > $endYear) {
+            $errors[] = 'L\'année doit être une valeur comprise entre ' . $startYear . ' and ' . $endYear;
+        }
+
+        return $errors ?? [];
+    }
+
 }
--- src/Model/WineManager.php
+++ src/Model/WineManager.php
@@ -24,4 +24,15 @@ class WineManager extends AbstractManager
     {
         parent::__construct(self::TABLE);
     }
+
+    public function insert(array $data) :void
+    {
+        $query = 'INSERT INTO ' . self::TABLE . ' (name, producer, year) VALUES (:name, :producer, :year)';
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('name', $data['name'], \PDO::PARAM_STR);
+        $statement->bindValue('producer', $data['producer'], \PDO::PARAM_STR);
+        $statement->bindValue('year', $data['year'], \PDO::PARAM_INT);
+
+        $statement->execute();
+    }
 }
